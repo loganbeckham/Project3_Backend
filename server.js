586b382b7747e576c285e248ba4bcbff62ad30a3
loganbeckham <loganbeckham1@gmail.com>
2022-12-09 14:46:41 -0600

    Heroku fix

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express')
 // const mongoose = require('mongoose')
 const cors = require('cors')
+require('dotenv').config()
 
 const app = express();
 
 app.use(express.json())
 app.use(cors())
 
-if(process.env.PORT) {
-    PORT = process.env.PORT
-}
+
+
 
 app.get('/', (req, res) => {
     res.send('hi');
@@ -20,6 +20,9 @@ app.get('/', (req, res) => {
 //              LISTENER
 // =======================================
 
+if(process.env.PORT) {
+    PORT = process.env.PORT
+}
 
 app.listen(PORT, () => {
     console.log(`App listening on port: 3000`)
