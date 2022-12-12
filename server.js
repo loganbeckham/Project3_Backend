1f187f46c8ad3dec9c675f0d1f6ea71ba90d063d
loganbeckham <loganbeckham1@gmail.com>
2022-12-12 10:15:09 -0600

    Attempt to fix Heroku Error

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
-// mongoose.set('strictQuery', true)
+mongoose.set('strictQuery', true)
 
 
 
