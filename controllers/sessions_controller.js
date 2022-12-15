3d1e6d840bba47bbd672793a4fa460df358c7d77
Matthew Chin <matthewchin@matthews-air.lan>
2022-12-15 03:06:10 -0500

    Finish login backend

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -4,7 +4,7 @@ const sessions = express.Router()
 const User = require('../models/users.js')
 
 sessions.get('/new', (req, res) => {
-  res.render('sessions/new.ejs', { currentUser: req.session.currentUser })
+    res.send(req.session.currentUser)
 })
 
 // on sessions form submit (log in)
@@ -18,7 +18,8 @@ sessions.post('/', (req, res) => {
       res.send('oops the db had a problem')
     } else if (!foundUser) {
       // if found user is undefined/null not found etc
-      res.send('<a  href="/">Sorry, no user found </a>')
+      req.session.currentUser = 'no user'
+      res.send(req.session.currentUser)
     } else {
       // user is found yay!
       // now let's check if passwords match
@@ -26,7 +27,7 @@ sessions.post('/', (req, res) => {
         // add the user to our session
         req.session.currentUser = foundUser
         // redirect back to our home page
-        res.redirect('/')
+        res.send(req.session.currentUser)
       } else {
         // passwords do not match
         res.send('<a href="/"> password does not match </a>')
@@ -37,7 +38,7 @@ sessions.post('/', (req, res) => {
 
 sessions.delete('/', (req, res) => {
   req.session.destroy(() => {
-    res.redirect('/')
+    res.send(req.session.currentUser)
   })
 })
 
